4ad423c8746d14a192e0a9320230056a09f7098d
Andreas Heissenberger <andreas@heissenberger.at>
2024-09-02 09:31:48 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mantine/core";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Button>Hello Mantine!</Button>
+        </div>
+    )
+}
